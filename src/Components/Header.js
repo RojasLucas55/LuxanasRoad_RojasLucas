2dbce0f69947b6b3c51cb83139c3a0dffdc0e331
RojasLucas55 <87238320+RojasLucas55@users.noreply.github.com>
2021-12-02 20:58:14 -0300

    Desafio NavBarJS

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar"
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Luxana's Road</h1>
+            <NavBar/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <a href=" # "> Home </a>
+            <a href=" # "> Productos </a>
+            <a href=" # "> Nosotros </a>
+            <a href=" # "> FAQ's</a>
+        </nav>
+    )
+}
+
+export default NavBar
