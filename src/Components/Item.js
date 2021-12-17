61819446cb4e946a8ab1a27e81f25a83d1c68eba
RojasLucas55 <87238320+RojasLucas55@users.noreply.github.com>
2021-12-17 19:25:32 -0300

    desafiocatalogo

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,14 +1,18 @@
 import { Card } from "react-bootstrap";
-import Contador from "./ItemCount";
+import ItemCount from "./ItemCount";
 const Item = ({ product }) => {
+
+    const handleOnAdd = () => {
+        console.log("Agregaste el producto al carrito")
+    }
     return (
         <div className="ItemBox">
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={product.imgURL} />
                 <Card.Body>
                     <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>{product.price}</Card.Text>
-                    <Contador/>
+                    <Card.Text><p>Precio: {product.price}</p></Card.Text>
+                    <ItemCount stock={product.stock} onAdd={handleOnAdd} initial={1} />
                 </Card.Body>
             </Card>
         </div>
--- src/Components/ItemCount.js
+++ src/Components/ItemCount.js
@@ -1,27 +1,31 @@
 import { useState } from "react"
 import Button from 'react-bootstrap/Button';
-const Contador = () => {
+const ItemCount = ({ stock, onAdd, initial }) => {
 
-    let init = 0
-    const [contador, setContador] = useState(init)
+    const [count, setCount] = useState(1);
 
-    const aumentarContador = () => {
-        setContador(contador+1)
+    const handleMinus = () => {
+        if (count > initial) {
+            setCount(count - 1)
+        }
     }
 
-    const restarContador=()=>{
-        setContador(contador-1)
-    } 
+    const handleAdd = () => {
+        if (count < stock) {
+            setCount(count + 1)
+        }
+    }
 
     return (
         <div id="contador-card">
-            <Button variant="primary" onClick={aumentarContador}>+</Button>
-            <p> {contador} </p>
-            <Button variant="danger" onClick={restarContador}>-</Button>
-            <Button variant="dark"> Add to Cart</Button>
-            
+                <Button variant="danger" onClick={handleMinus}> - </Button>
+                <p> {count} </p>
+                <Button variant="primary" onClick={handleAdd}> + </Button>
+
+                <Button variant="dark" onClick={() => onAdd()} type="button"> Add to Cart</Button>
+
         </div>
     )
 }
 
-export default Contador
+export default ItemCount
